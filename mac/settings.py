d984cc798fd89bf24bad35a9b50cdac5191c4d46
Rschanania <chananias1@gmail.com>
2020-04-27 16:42:59 +0530

    lol

--- mac/settings.py
+++ mac/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 
 
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
