fb3c58934122020be6e0e34ba9b6aecb64acc985
Rschanania <chananias1@gmail.com>
2020-04-27 16:30:51 +0530

    root is added

--- mac/settings.py
+++ mac/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiels')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 MEDIA_URL='/media/'
 
 
